26ea75683bdae2a93dc8ab197a11508357252168
Сергей Цымбалов <106691442+Serjio99@users.noreply.github.com>
2024-04-05 11:16:57 +0300

    Add files via upload

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,11 +3,7 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleServiceImpl;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
@@ -15,87 +11,62 @@ import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-
 
     private final UserServiceImpl userService;
     private final RoleServiceImpl roleService;
 
-    @Autowired
+    //    @Autowired
     public AdminController(UserServiceImpl userService, RoleServiceImpl roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @GetMapping("/admin")   // получаем всех пользователей и отображаем страницу со списком пользователей
-    public String getAllUsersPage(Model model) {
+    @GetMapping()   // отображение списка всех пользователей
+    public String getAllUsers(Model model) {
         model.addAttribute("users", userService.getAllUsers());
         return "admin";
     }
 
-    @GetMapping("/admin/show")  // получаем пользователя по указанному идентификатору и отображаем информацию о нем.
+    @GetMapping("/show")  // получаем пользователя по указанному идентификатору и отображаем информацию о нем.
     public String getUserById(@RequestParam("id") Long id, Model model) {
         model.addAttribute("user", userService.findUserById(id));
         return "user";
     }
 
-    @GetMapping("/admin/new")   //  отображаем форму для создания нового пользовател
+    @GetMapping("/new")   //  ?отображаем форму для создания нового пользовател
     public String createUserFrom(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("roles", roleService.getListOfRoles());
         return "newUserCreate";
     }
 
-    @GetMapping("/admin/edit")  // форма для редактирования пользователя
+    @PostMapping("/addUser")  //  добавляем нового пользователя.
+    public String addNewUser(@ModelAttribute("user") @Valid User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roleService.getListOfRoles());
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit")  // форма для редактирования пользователя
     public String editUserById(@RequestParam(value = "id") Long id, Model model) {
         model.addAttribute("user", userService.findUserById(id));
         model.addAttribute("roles", roleService.getListOfRoles());
         return "editUser";
     }
 
-    @PostMapping("/admin/addUser")
-    public String processAddUserForm(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            return showNewUserForm(user, model);
-        }
-        saveUserAndRedirect(user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/admin/save")
-    public String processEditUserForm(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            return showEditUserForm(user, model);
-        }
-        updateUserAndRedirect(user);
-        return "redirect:/admin";
-    }
-
-    private String showNewUserForm(User user, Model model) {
-        model.addAttribute("user", user);
-        model.addAttribute("roles", roleService.getListOfRoles());
-        return "newUserCreate";
-    }
-
-    private void saveUserAndRedirect(User user) {
-        user.setPassword(user.getPassword());
-        userService.addUser(user);
-    }
-
-    private String showEditUserForm(User user, Model model) {
-        model.addAttribute("user", user);
-        model.addAttribute("roles", roleService.getListOfRoles());
-        return "editUser";
-    }
-
-    private void updateUserAndRedirect(User user) {
-        user.setPassword(user.getPassword());
+    @PostMapping("/save")     // сохраняем данные пользователя
+    public String saveUser(@ModelAttribute("user") @Valid User user) {
         userService.editUserById(user);
+        return "redirect:/admin";
     }
 
-    @GetMapping(value = "/admin/delete")
+    @GetMapping(value = "/delete")
     public String deleteUser(@RequestParam("id") Long id) {
         userService.removeUserById(id);
         return "redirect:/admin";
     }
 }
+
